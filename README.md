5c8cc90d602624ca03b1826fe8adf30f4f86e0ba
dpy1123 <dpy1123@126.com>
2017-09-01 19:35:00 +0800

    use AppKit on macOS

--- README.md
+++ README.md
@@ -9,11 +9,14 @@ tested on:
 
 ## setup
 ```bash
-pip3 intall pyqt5
+pip3 install -U pyqt5 pyobjc
 ```
 
 ## run
 1. run `python3 gif4wx.py` in shell
 2. copy on source gif
-3. (only on macOS) activate py window
-4. paste wherever you want
+3. paste wherever you want
+
+## change log
+[09-01]  
+ Use AppKit on macOS, avoid activate python window for Qt to get clipboard change event.
--- gif4wx.py
+++ gif4wx.py
@@ -1,13 +1,16 @@
 import os
 import sys
+import platform
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
 
-def on_clipboard_change(clipboard):
+def on_clipboard_change(clipboard, log=False):
     mine_data = clipboard.mimeData()
     urls = mine_data.urls()
     if len(urls) > 0 and len(urls) == len([url for url in urls if get_file_suffix(url.toString()) == '.gif']):
+        if log:
+            print(urls)
         clipboard.dataChanged.disconnect(func)
         mimeData = QMimeData()
         mimeData.setUrls(urls)
@@ -19,12 +22,39 @@ def get_file_suffix(path):
     return os.path.splitext(path)[1].lower()
 
 
+def pasteboard_polling_for_mac(log=False):
+    import AppKit
+    import time
+    from urllib.parse import unquote
+
+    count = 0
+    while True:  # why polling? see: https://stackoverflow.com/questions/5033266/can-i-receive-a-callback-whenever-an-nspasteboard-is-written-to?answertab=votes#tab-top
+        pb = AppKit.NSPasteboard.generalPasteboard()
+        url = pb.dataForType_(AppKit.NSURLPboardType)
+        if url is not None and pb.changeCount() > count:
+            url = url.bytes().tobytes().decode()
+            url = url[url.index('<string>') + len('<string>'): url.index('</string>')]
+            url = unquote(url)
+            if url.startswith('file://') and get_file_suffix(url) == '.gif':
+                if log:
+                    print(url)
+                pb.clearContents()
+                nsurl = AppKit.NSURL.fileURLWithPath_isDirectory_(url[len('file://'):], False)
+                pb.writeObjects_([nsurl])
+                count = pb.changeCount()
+        time.sleep(1)
+
+
 if __name__ == '__main__':
+    
+    if platform.system() == 'Darwin':
+        pasteboard_polling_for_mac()
+    else:
+        app = QApplication([])
+        app.setQuitOnLastWindowClosed(False)
 
-    app = QApplication([])
-    clipboard = app.clipboard()
-    func = lambda: on_clipboard_change(clipboard)
-    clipboard.dataChanged.connect(func)
-    app.setQuitOnLastWindowClosed(False)
+        clipboard = app.clipboard()
+        func = lambda: on_clipboard_change(clipboard)
+        clipboard.dataChanged.connect(func)
 
-    sys.exit(app.exec())
+        sys.exit(app.exec())
